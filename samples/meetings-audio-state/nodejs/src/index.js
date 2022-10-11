099227d382212507f101b77dbb9292af3360c75a
Stephane Lefevre <stephane.lefevre@public-i.info>
2022-10-11 10:49:05 +0100

    audio mute app changes
    don't forget to set the ngrok url in manifest

--- samples/meetings-audio-state/nodejs/src/index.js
+++ samples/meetings-audio-state/nodejs/src/index.js
@@ -1,7 +1,7 @@
 import { Provider, teamsTheme } from '@fluentui/react-northstar';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './style/style.css';
 import { AppRoute } from './router/router';
 
 ReactDOM.render(
